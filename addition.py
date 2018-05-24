42b655826cffab50f46fbc3d4acea8cf94a3f554
rijutawankar <rujuta.w1992@gmail.com>
2018-05-24 13:54:27 +0530

    code for addition

---
+++ addition.py
@@ -0,0 +1,15 @@
+
+def adder(c,d):
+    z=c+d
+    return z
+a=25
+b=20
+def subtract(x,y):
+     return x-y
+
+val = adder(a,b)
+sub = subtract(a,b)
+
+print ("The value of the function is",val)
+print ("The value of the function is",sub)
+print ("The value of the function is" + str(val)+ "subtract returns" +str(sub))
